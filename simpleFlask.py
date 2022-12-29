94bf59da67df52de7e7b305a020ffde550a72ca9
KimBumSoo <show7441@naver.com>
2022-12-29 17:18:49 +0900

    flask_test

--- simpleFlask.py
+++ simpleFlask.py
@@ -1,7 +1,21 @@
-a=10
-b=20
-c=a+b
+#필요한 모듈을 단계적으로 import
+from flask import Flask
 
-print(c)
+app = Flask(__name__)
 
-print('Hello Flask')
+# ~~/data?temp=10&humi=20
+
+# 테스트 기능
+@app.route('/hello')
+def hello_Flask():
+    return 'Hello, Flask'
+
+@app.route('/hi')
+def hi_Flask():
+    return 'Hi, Flask'
+
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
