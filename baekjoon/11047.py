29d00780dc52aefa8b1de09da1e104ca3b99cd3b
Kimbumsoo99 <show7441@naver.com>
2023-05-22 14:54:40 +0900

    11047 동전 문제 풀이

---
+++ baekjoon/11047.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+coins = []
+for i in range(0, N):
+    coins.append(int(input()))
+
+coins.sort()
+
+
+count = 0
+# coins.pop()
+coin = coins.pop()
+while K:
+    # if K % coin == 0:
+    #    count += K // coin
+    #    break
+    if K >= coin:
+        count += K // coin
+        K -= (K // coin) * coin
+        # K -= coin
+        # count += 1
+    else:
+        coin = coins.pop()
+print(count)
