53b5c276384f7ed43d8cd9fb592dc4ea96ff3203
Kimbumsoo99 <show7441@naver.com>
2023-05-23 03:27:14 +0900

    9012

--- baekjoon/stack/9012.py
+++ baekjoon/stack/9012.py
@@ -1,28 +1,29 @@
 import sys
+
 input = sys.stdin.readline
 
-T=int(input())
-stack=[]
-arr=[]
-check=0
-for i in range(0,T):
-    arr+=input().split()
+T = int(input())
+stack = []
+arr = []
+check = 0
+for i in range(0, T):
+    arr += input().split()
 
 for i in arr:
-    #print("i=",i)
+    # print("i=",i)
     for index, a in enumerate(i):
-        if a=='(':
+        if a == '(':
             stack.append(a)
-            #print(stack)
-        elif a==')':
-            if(len(stack)<1):
-                check=-1
+            # print(stack)
+        elif a == ')':
+            if len(stack) < 1:
+                check = -1
                 break
             stack.pop()
-            #print(stack)
-    if len(stack)>0 or check==-1:
+            # print(stack)
+    if len(stack) > 0 or check == -1:
         print("NO")
-        check=0
+        check = 0
     else:
         print("YES")
-    stack=[]
+    stack = []
