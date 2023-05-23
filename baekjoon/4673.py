1364cf6442d601067da78c59f7f150002622898d
Kimbumsoo99 <show7441@naver.com>
2023-05-23 17:06:47 +0900

    4673 훨씬 빠른 정답

--- baekjoon/4673.py
+++ baekjoon/4673.py
@@ -1,22 +1,25 @@
-N = 10000
+self_number = []
+all_number = set()
+not_self_number = set()
 
-def isNum(i, num):
-    result = i
-    while i > 0:
-        tmp = i % 10
-        i = i // 10
-        result += tmp
-    if num == result:
-        return True
+
+def notSelfNum(current_num):
+    value = current_num
+    while current_num > 0:
+        value += current_num % 10
+        current_num //= 10
+    if value < 10001:
+        return value
     else:
-        return False
+        return
 
 
-for num in range(1, N + 1):
-    check = True
-    for i in range(num - 1, 0, -1):
-        if isNum(i, num):
-            check = False
-            break
-    if check:
-        print(num)
+for i in range(1, 10001):
+    all_number.add(i)
+    num = notSelfNum(i)
+    not_self_number.add(num)
+
+self_number = sorted(all_number - not_self_number)
+
+for i in self_number:
+    print(i)
