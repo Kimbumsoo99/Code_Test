b7551b41cb2832005d3160f941bc038d8553e360
Kimbumsoo99 <show7441@naver.com>
2023-05-30 05:07:06 +0900

    DFS, BFS 예시

---
+++ baekjoon/bfs.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+adj = [[0] * 13 for _ in range(13)]
+adj[0][1] = adj[0][2] = 1
+adj[1][3] = adj[1][4] = 1
+adj[3][7] = adj[3][8] = 1
+adj[4][9] = 1
+adj[2][5] = adj[2][6] = 1
+adj[6][10] = adj[6][11] = adj[6][12] = 1
+
+# for row in adj:
+#     print(row)
+
+def bfs():
+    dq = deque()
+    dq.append(0)
+    while dq:
+        now = dq.popleft()
+        for nxt in range(13):
+            if adj[now][nxt]:
+                dq.append(nxt)
+
+bfs()
