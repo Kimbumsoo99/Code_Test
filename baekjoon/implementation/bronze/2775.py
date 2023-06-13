fe9980424847c25ca78042e3f9e862e317576536
Kimbumsoo99 <show7441@naver.com>
2023-06-13 13:41:37 +0900

    2775 아파트 층, 호 문제 해결 X

---
+++ baekjoon/implementation/bronze/2775.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+# T = int(input())
+T = 1
+
+
+for i in range(T):
+    K = int(input())  # 층 (행)
+    N = int(input())  # 호 (열)
+    apart = [1 for a in range(K)]
+
+    print(apart)
+
