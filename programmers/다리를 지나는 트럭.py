07057146f5a2ae84128d521e65cbe60cac582a8c
Kimbumsoo99 <show7441@naver.com>
2023-06-28 04:02:52 +0900

    문제 풀이 0628

--- programmers/다리를 지나는 트럭.py
+++ programmers/다리를 지나는 트럭.py
@@ -4,31 +4,21 @@ from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     bridge = deque()
-    bridge_count = deque()
+    bridge_count = []
+    tmp = 0
     while True:
         answer += 1
-        tmp = truck_weights[0] if len(truck_weights) > 0 else 0
         for i in range(len(bridge_count)):
             bridge_count[i] += 1
-        if len(bridge) != 0 and sum(bridge) + tmp > weight:
-            if bridge_count[0] > bridge_length:
-                bridge.popleft()
-                bridge_count.popleft()
-                answer -= 1
-                if len(truck_weights) == 0 and len(bridge_count) < 1:
-                    break
-        else:
-            for i in range(len(bridge_count)):
-                bridge_count[i] += 1
-            if len(bridge) != 0 and bridge_count[0] > bridge_length:
-                bridge.popleft()
-                bridge_count.popleft()
-            if tmp > 0:
-                bridge.append(truck_weights.pop(0))
-                bridge_count.append(1)
-        print(bridge, bridge_count)
-        print(truck_weights)
+        if len(bridge_count) > 0 and bridge_count[0] == bridge_length + 1:
+            bridge.popleft()
+            bridge_count.pop(0)
+            if len(bridge_count) == 0 and len(truck_weights) == 0:
+                return answer
 
-    return answer - 1
+        if len(truck_weights) > 0:
+            tmp = truck_weights[0]
 
-print(solution(2, 10, [7,4,5,6]))
+        if len(truck_weights) > 0 and sum(bridge) + tmp <= weight:
+            bridge.append(truck_weights.pop(0))
+            bridge_count.append(1)
